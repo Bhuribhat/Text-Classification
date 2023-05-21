11e862e4f78df399af67adf3e6f60718f7f92afe
Bhuribhat <Bhuribhat@gmail.com>
2023-05-21 17:39:43 +0700

    fix err

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
-from app.model.detection_model import predict_pipeline
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from model.detection_model import predict_pipeline
 
 
 class TextIn(BaseModel):
@@ -12,14 +14,21 @@ class PredictionOut(BaseModel):
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def home():
+def home(request: Request):
     return {"health_check": "OK"}
 
 
 @app.post("/predict", response_model=PredictionOut)
-def predict(payload: TextIn):
+def predict(request: Request, payload: TextIn):
     language = predict_pipeline(payload.text)
-    return {"language": language}
+    return templates.TemplateResponse("index.html", {"request": request, "language": language})
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
